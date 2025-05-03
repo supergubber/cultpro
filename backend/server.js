af62b0b8f834225ab52a3141ba5a5ac8cf23f747
rizwankhan <super047240@gmail.com>
2025-05-03 22:42:48 +0530

    all this

--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,11 @@
 const express = require('express')
 const connectDB = require('./config/db')
+require('dotenv').config()
 const authRouter = require('./routes/authRoute')
 const cors = require('cors')
 const path = require('path')
 const cookieParser = require('cookie-parser')
-require('dotenv').config()
-
+const _dirname = path.resolve()
 const app = express()
 const PORT = process.env.PORT || 5000
 
@@ -21,15 +21,12 @@ app.use(cookieParser())
 
 app.use('/api/v1/auth', authRouter)
 
-if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, '/frontend/dist')))
-
-  app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'frontend', 'dist', 'index.html'))
-  })
-}
+app.use(express.static(path.join(_dirname, '/frontend/dist')))
+app.get('*', (_, res) => {
+  res.sendFile(path.resolve(_dirname, 'frontend', 'dist', 'index.html'))
+})
 
 app.listen(PORT, () => {
-  console.log(`Server is running on port:${PORT}`)
+  console.log(`Server started at http://localhost:${PORT}`)
   connectDB()
 })
